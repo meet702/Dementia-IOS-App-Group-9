d8031cc013a38e5832361fc112231655222b6095
meet702 <meetchhabhaiya10@gmail.com>
2026-01-21 09:26:01 +0530

    Changed the font according to the HIG

--- IOS-App/Controller/Memory Response/ResponseViewController.swift
+++ IOS-App/Controller/Memory Response/ResponseViewController.swift
@@ -120,7 +120,8 @@ class ResponseViewController: UIViewController, UITableViewDelegate, UITableView
         
         let label = UILabel()
         label.text = "People in this memory"
-        label.font = UIFont.boldSystemFont(ofSize: 17)
+//        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.font = .preferredFont(forTextStyle: .headline)
         label.textColor = UIColor.black
         label.translatesAutoresizingMaskIntoConstraints = false
         
